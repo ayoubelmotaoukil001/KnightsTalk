dc67dd502eb2cded85ba8e81d7c4a12458cb20cf
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-04-08 02:05:28 +0100

    feat (User) edit: add model relationships

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,48 +2,35 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Database\Factories\UserFactory;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class User extends Authenticatable
-{
-    /** @use HasFactory<UserFactory> */
+
+class User extends Authenticatable {
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
-    protected $fillable = [
-        'name',
-        'email',
-        'password',
-    ];
+    protected $fillable = ['name', 'email', 'password', 'is_admin'];
+    protected $hidden = ['password', 'remember_token'];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
-
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
-    protected function casts(): array
+    public function progress()
     {
-        return [
-            'email_verified_at' => 'datetime',
-            'password' => 'hashed',
-        ];
+        return $this->hasMany(Progress::class);
+    }
+    public function analyses()
+    {
+        return $this->hasMany(Analysis::class);
+    }
+    public function courses()
+    {
+        return $this->hasMany(Course::class);
+    }
+    public function messages()
+    {
+        return $this->hasMany(Message::class);
+    }
+    public function puzzles()
+    {
+        return $this->hasMany(Puzzle::class);
     }
 }
