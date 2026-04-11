06434cb1ef13cdfb74813bad4f1161f7978aff85
ayoubelmotaoukil001 <ayoubelmotaoukil001@gmail.com>
2026-04-11 01:10:26 +0100

    feat (routes) add: admin puzzle resource routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\PuzzleController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +16,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function () {
+        Route::get('puzzles', [PuzzleController::class, 'index'])->name('puzzles.index');
+        Route::get('puzzles/create', [PuzzleController::class, 'create'])->name('puzzles.create');
+        Route::post('puzzles', [PuzzleController::class, 'store'])->name('puzzles.store');
+        Route::delete('puzzles/{puzzle}', [PuzzleController::class, 'destroy'])->name('puzzles.destroy');
+    });
 });
 
 require __DIR__.'/auth.php';
